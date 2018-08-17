e9d329f49bf5c10fe0b31de4f940721163c540d0
Bill Chatfield <bill.chatfield@cardinalhealth.com>
2018-08-17 13:41:51 -0400

    Updated addcert to accept command line parameters

--- bin/addcert.bat
+++ bin/addcert.bat
@@ -3,24 +3,52 @@
 setlocal
 
 rem
-rem Addes a certificate to the keystore for JAVA_HOME.
+rem Addes certificates to the keystore for the Java instance defined by JAVA_HOME.
 rem
 
-set CERT_ALIAS=CommerceDummyCertificate
-set CERT_FILE=CommerceDummyCertificate.crt
+set PROGRAM_NAME=%~n0
 
-echo It may be necessary to "Run as Administrator" because recent versions of 
-echo Windows protect the directory tree where the keystore file is stored.
-echo.
+if "%*"=="" (
 
-"%JAVA_HOME%"\bin\keytool -importcert -trustcacerts ^
-    -keystore "%JAVA_HOME%\jre\lib\security\cacerts" ^
-    -alias "%CERT_ALIAS%" ^
-    -file "%CERT_FILE%" ^
-    -storepass changeit
+    call :msg Please provide one or more certificate files on the command line.
+    
+) else (
+
+    if not defined JAVA_HOME (
+
+        call :msg The environment variable JAVA_HOME must be set.
+
+    ) else (
+
+        call :msg It may be necessary to "Run as Administrator" because recent versions of 
+        call :msg Windows protect the directory tree where the keystore file is stored.
+        echo.
+
+        for %%c in (%*) do (
+            "%JAVA_HOME%\bin\keytool" -importcert -trustcacerts ^
+                -keystore "%JAVA_HOME%\jre\lib\security\cacerts" ^
+                -alias "%%~nc" ^
+                -file %%c ^
+                -storepass changeit
+        )
+    )
+)
 
 endlocal
 
-echo.
-pause
+rem Pause only if started from the desktop.
+for /f "tokens=2" %%a in ("%CMDCMDLINE%") do (
+    if "%%a"=="/c" (
+        echo.
+        pause
+    )
+)
 
+rem This is the end. My only friend, the end.
+goto :EOF
+
+rem Subroutine to print a message.
+:msg
+    echo %PROGRAM_NAME%: %*
+    exit /b
+ 
