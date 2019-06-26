a56081b285cc084518fdb04c78750a4a833e93b0
Bill Chatfield <bill_chatfield@yahoo.com>
2019-06-26 00:52:00 -0400

    Put files back in the right places

--- bin/mvn-gen-eclipse-conf.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-for %%j in ("%~dp0..\lib\bsh*.jar") do set BSH_JAR=%%j
-set BSH_SCRIPT=%~dp0..\scripts\%~n0.bsh 
-java -classpath "%BSH_JAR%" bsh.Interpreter %BSH_SCRIPT% %*
-endlocal
--- lib/gcj-setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set PATH=C:\mingw\bin;%PATH%
-set CLASSPATH=c:\mingw\share\java\libgcj-3.4.5.jar
-
-gcj -dumpversion
-where gcj
-where gij
-echo CLASSPATH=%CLASSPATH%
