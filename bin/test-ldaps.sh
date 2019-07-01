7933c8568fbc51050cc29f3014fe61c74fa8e7c3
Chatfield <bill.chatfield@lsec5009dmwebapp01.c.datamarketstg-pr-cah.internal>
2019-07-01 16:37:01 +0000

    Added debugging

--- bin/test-ldaps.sh
+++ bin/test-ldaps.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+LDAPS_HOST=ldap-ec500.cardinalhealth.net
+LDAPS_PORT=636
+
 BIN_DIR=$(dirname "$0") && [ "$BIN_DIR" = "." ] && BIN_DIR=$(pwd)
+CLASSES_DIR=$(dirname "$BIN_DIR")/classes
 JAVA=java
 
 if [ -n "$JAVA_HOME" ]
@@ -9,7 +13,7 @@ then
 	read ANSWER
 	if [ "$ANSWER" = "y" ]
 	then
-		JAVA="$JAVA_HOME"\bin\java
+		JAVA="$JAVA_HOME"/bin/java
 	else
         	echo Will use java.home property instead.
 	fi
@@ -17,4 +21,33 @@ fi
 
 "$JAVA" -XshowSettings:properties -version 2>&1 | grep "    java.home"
 
-exec "$JAVA" -cp $(dirname "$BIN_DIR")/classes SSLPoke ldap-ec500.cardinalhealth.net 636
+OPTS=-Djavax.net.ssl.trustStore=$HOME/.keystore
+OPTS+=" -Djavax.net.debug=ssl"
+
+exec "$JAVA" "$OPTS" -cp "$CLASSES_DIR" SSLPoke $LDAPS_HOST $LDAPS_PORT
+
+# Values for javax.net.debug:
+#
+# all            turn on all debugging
+# ssl            turn on ssl debugging
+#
+# The following can be used with ssl:
+#
+#    record       enable per-record tracing
+#    handshake    print each handshake message
+#    keygen       print key generation data
+#    session      print session activity
+#    defaultctx   print default SSL initialization
+#    sslctx       print SSLContext tracing
+#    sessioncache print session cache tracing
+#    keymanager   print key manager tracing
+#    trustmanager print trust manager tracing
+#    pluggability print pluggability tracing
+#
+#    handshake debugging can be widened with:
+#    data         hex dump of each handshake message
+#    verbose      verbose handshake message printing
+#
+#    record debugging can be widened with:
+#    plaintext    hex dump of record plaintext
+#    packet       print raw SSL/TLS packets
