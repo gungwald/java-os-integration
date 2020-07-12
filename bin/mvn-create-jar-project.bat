3841608009e6f8b3c415ef0028503e85798d2615
Bill Chatfield <bill_chatfield@yahoo.com>
2020-07-12 02:22:07 -0400

    Made mvn-create-jar-project work with Java 1.4

--- bin/mvn-create-jar-project.bat
+++ bin/mvn-create-jar-project.bat
@@ -1,7 +1,1 @@
-@echo off
-
-set /p GROUP_ID=Enter groupId [Ex: com.cardinalhealth.bdcoe]: 
-set /p ARTIFACT_ID=Enter atrifactId [Ex: table-metadata-import-tool]:
-
-mvn archetype:generate -DgroupId="%GROUP_ID%" -DartifactId="%ARTIFACT_ID%" ^
--DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+@java -classpath %~dp0../classes MavenJarProjectCreator %*
--- src/MavenJarProjectCreator.java
+++ src/MavenJarProjectCreator.java
@@ -2,43 +2,40 @@ import java.io.*;
 
 public class MavenJarProjectCreator {
 
-    public static final int EXIT_SUCCESS = 0;
-    public static final int EXIT_FAILURE = 1;
+	public static final int EXIT_SUCCESS = 0;
+	public static final int EXIT_FAILURE = 1;
 
-    public static void main(String[] args) {
-        int exitCode = EXIT_SUCCESS;
+	public static void main(String[] args) {
+		int exitCode = EXIT_SUCCESS;
 
-        try 
-        {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.print("Enter groupId (example: com.companyname.groupname): ");
-            String group = in.readLine();
+			System.out.print("Enter groupId (example: com.companyname.groupname): ");
+			String group = in.readLine();
 
-            if (group != null && group.length() > 0) {
-                System.out.print("Enter artifactId (example: my-component-name): ");
-                String artifact = in.readLine();
+			if (group != null && group.length() > 0) {
+				System.out.print("Enter artifactId (example: my-component-name): ");
+				String artifact = in.readLine();
 
-                if (artifact != null && artifact.length() > 0) {
-                    String[] cmd = new String[]
-                    {
-                            "mvn", 
-                            "archetype:generate",
-                            "-DgroupId=" + group,
-                            "-DartifactId=" + artifact,
-                            "-DarchetypeArtifactId=maven-archetype-quickstart",
-                            "-DinteractiveMode=false"
-                    };
-                    exitCode = new ProcessBuilder(cmd).inheritIO().start().waitFor();
-                }
-            }
-        } 
-        catch (Exception e) 
-        {
-            System.err.println(e.getLocalizedMessage());
-            exitCode = EXIT_FAILURE;
-        }
-        System.exit(exitCode);
-    }
+				if (artifact != null && artifact.length() > 0) {
+					String[] cmd = new String[]{
+							"mvn",
+							"archetype:generate",
+							"-DgroupId=" + group,
+							"-DartifactId=" + artifact,
+							"-DarchetypeArtifactId=maven-archetype-quickstart",
+							"-DinteractiveMode=false"
+					};
+					exitCode = new Executor().exec(cmd);
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getLocalizedMessage());
+			exitCode = EXIT_FAILURE;
+		}
+		System.exit(exitCode);
+	}
+
 }
 
